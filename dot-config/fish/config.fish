ba1d1586fb5a6da232ec576068e31b8ccb8ca2a5
nephin <nephi.norton@gmail.com>
2024-12-31 19:59:55 -0700

    Hackily set fish as login shell; fixed temp function

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -1,3 +1,5 @@
+fish_add_path ~/.cargo/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr -a ls eza -lh
@@ -6,12 +8,9 @@ if status is-interactive
     abbr -a ll la
     abbr -a grep grep --color=auto
 
-    abbr -a temp cd $(mktemp -d)
-
     # --universal --export
     set -Ux EDITOR vim
 
-    # source $HOME/.cargo/env
     starship init fish | source
 end
 
